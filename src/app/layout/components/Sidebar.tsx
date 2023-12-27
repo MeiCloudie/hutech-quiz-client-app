cc187c935a2d1aa5a68321b9f34f8ec64e0215cb
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-12-27 22:48:25 +0700

    feat(layout): Create Sidebar component

---
+++ src/app/layout/components/Sidebar.tsx
@@ -0,0 +1,153 @@
+import * as React from "react"
+import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles"
+import MuiDrawer from "@mui/material/Drawer"
+import List from "@mui/material/List"
+import Divider from "@mui/material/Divider"
+import IconButton from "@mui/material/IconButton"
+
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
+import ChevronRightIcon from "@mui/icons-material/ChevronRight"
+
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+
+import HomeIcon from "@mui/icons-material/Home"
+import PersonIcon from "@mui/icons-material/Person"
+
+import { Link, useLocation } from "react-router-dom"
+
+const drawerWidth = 240
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+})
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+})
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}))
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}))
+
+const pages = [
+  {
+    text: "Trang Chủ",
+    link: "/home",
+    icon: <HomeIcon />,
+  },
+  {
+    text: "Hồ Sơ",
+    link: "/profiles",
+    icon: <PersonIcon />,
+  },
+  //TODO: Thêm menu ở đây!
+]
+
+interface SidebarProps {
+  open: boolean
+  handleDrawerClose: () => void
+}
+
+const Sidebar = (props: SidebarProps) => {
+  const theme = useTheme()
+  const location = useLocation()
+  return (
+    <Drawer
+      variant="permanent"
+      open={props.open}
+      PaperProps={{
+        sx: {
+          backgroundColor: "#ffc80f",
+          color: "#101331",
+        },
+      }}
+    >
+      <DrawerHeader>
+        <IconButton onClick={props.handleDrawerClose}>
+          {theme.direction === "rtl" ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+      </DrawerHeader>
+
+      <Divider />
+
+      <List>
+        {pages.map((p, index) => (
+          <ListItem key={index} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: props.open ? "initial" : "center",
+                px: 2.5,
+                backgroundColor:
+                  location.pathname === p.link ? "#ffd964" : "inherit",
+              }}
+              component={Link}
+              to={p.link}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: props.open ? 3 : "auto",
+                  justifyContent: "center",
+                  color: "#101331",
+                }}
+              >
+                {p.icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={p.text}
+                sx={{ opacity: props.open ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+      <Divider />
+    </Drawer>
+  )
+}
+
+export default Sidebar
