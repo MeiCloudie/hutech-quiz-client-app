53b2cfe9f47413c33d12afb5b7284bc482c10578
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-12-27 22:40:58 +0700

    feat(layout): Refactor layout

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import ScrollToTopButton from "./components/ScrollToTopButton"
+
+function App() {
+  return (
+    <div className="App">
+      {/* Layout here! */}
+      <ScrollToTopButton />
+    </div>
+  )
+}
+
+export default App
---
+++ src/app/layout/components/ScrollToTopButton.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import Fab from "@mui/material/Fab";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { Box, Tooltip } from "@mui/material";
+
+const ScrollToTopButton: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.scrollY || document.documentElement.scrollTop;
+      setIsVisible(scrollTop > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <Box sx={{ position: "fixed", bottom: 20, right: 20, zIndex: 1000 }}>
+      {isVisible && (
+        <Tooltip title="Lên đầu trang" placement="left" arrow>
+          <Fab color="primary" aria-label="Scroll to top" onClick={scrollToTop}>
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </Tooltip>
+      )}
+    </Box>
+  );
+};
+
+export default ScrollToTopButton;
