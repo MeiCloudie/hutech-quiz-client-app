020dd42c5c670fbb41e439122952b77b5bbcb9f1
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-12-27 23:14:55 +0700

    feat(models): Create Entity, Faculty, User model

---
+++ src/app/common/models/Entity.ts
@@ -0,0 +1,17 @@
+export interface BaseEntity<TId> {
+  id: TId
+}
+
+export default interface Entity extends BaseEntity<string> {}
+
+export interface Auditable {
+  createDate: Date
+}
+
+export interface BaseEntityFormValues<TId> {
+  id?: TId
+}
+
+export interface EntityFormValues {
+  id?: string
+}
---
+++ src/app/models/Faculty.ts
@@ -0,0 +1,27 @@
+import Entity, { Auditable, EntityFormValues } from "../common/models/Entity"
+
+export interface Faculty extends Entity, Auditable {
+  name: string
+}
+
+export class Faculty implements Faculty {
+  id: string = ""
+  createDate: Date = new Date()
+  name: string = ""
+
+  constructor(init?: FacultyFormValues) {
+    Object.assign(this, init)
+  }
+}
+
+export class FacultyFormValues implements EntityFormValues {
+  id?: string = ""
+  name: string = ""
+
+  constructor(faculty?: Faculty) {
+    if (faculty) {
+      const { createDate, ...rest } = faculty
+      Object.assign(this, rest)
+    }
+  }
+}
