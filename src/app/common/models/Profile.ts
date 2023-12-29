2f2d8e8646749af43cab772ac5a7f85f95a13c09
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-12-29 14:51:47 +0700

    feat(models): Add Profile model

---
+++ src/app/common/models/Profile.ts
@@ -0,0 +1,16 @@
+import { Faculty } from "../../models/Faculty"
+// import { Group } from "../../models/Group";
+import Entity from "./Entity"
+
+export default interface Profile extends Entity {
+  id: string
+  userName: string
+  email: string
+  firstName: string
+  lastName: string
+  avatarUrl?: string
+  class: string
+  faculty?: Faculty
+  // isSubmitted?: boolean,
+  // groups: Group[]
+}
