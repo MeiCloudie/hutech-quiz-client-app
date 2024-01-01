e94771293adf8814f9ebe0e6ae804dda2fe564fd
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-01-01 15:00:01 +0700

    feat(room): Add 2 button

--- src/features/rooms/list/RoomPage.tsx
+++ src/features/rooms/list/RoomPage.tsx
@@ -1,9 +1,24 @@
 import React from "react"
 import RoomList from "./RoomList"
+import { Box, Button } from "@mui/material"
+import AddCircleIcon from "@mui/icons-material/AddCircle"
+import SearchIcon from "@mui/icons-material/Search"
 
 const RoomPage = () => {
   return (
     <React.Fragment>
+      <Box sx={{ my: 1, display: "flex", justifyContent: "flex-end" }}>
+        <Button
+          variant="contained"
+          sx={{ mx: 1 }}
+          startIcon={<AddCircleIcon />}
+        >
+          TẠO PHÒNG
+        </Button>
+        <Button variant="contained" startIcon={<SearchIcon />}>
+          TÌM PHÒNG
+        </Button>
+      </Box>
       <RoomList />
     </React.Fragment>
   )
