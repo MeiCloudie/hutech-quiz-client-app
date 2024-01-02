e56061275f4d54efd08cd5c517e78e1d8c047315
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-01-02 20:52:29 +0700

    feat(result): Update result page

--- src/features/rooms/result/ResultPage.tsx
+++ src/features/rooms/result/ResultPage.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  LinearProgress,
   Paper,
   Table,
   TableBody,
@@ -10,7 +11,10 @@ import {
   Typography,
 } from "@mui/material"
 import { useStore } from "../../../app/stores/store"
-import { User } from "../../../app/models/User"
+import { Record } from "../../../app/models/Record"
+import { useEffect, useState } from "react"
+import { Room } from "../../../app/models/Room"
+import { useParams } from "react-router-dom"
 
 function createData(
   fullName: string,
@@ -23,13 +27,41 @@ function createData(
 }
 
 function ResultPage() {
-  const {roomStore} = useStore()
+  const { roomStore, quizSocketStore } = useStore()
+  const [room, setRoom] = useState<Room>(new Room())
+  const { roomId } = useParams<{ roomId: string }>()
 
-  const getRows = (users: User[]) => [
-    ...users.map((u, index) => ( 
-    createData(`${u.lastName} ${u.firstName}`, u.email, 10, 5, 80)
-    ))
-  ]
+  useEffect(() => {
+    if (roomId) {
+      roomStore.get(roomId, true).then(() => {
+        setRoom(roomStore.selectedItem ?? new Room())
+        console.log(roomStore.selectedItem)
+        const roomCode = roomStore.selectedItem?.code
+        if (roomCode) quizSocketStore.createHubConnection(roomCode)
+      })
+    }
+  }, [roomId, roomStore])
+
+  if (roomStore.isDetailsLoading)
+    return (
+      <Box sx={{ width: "100%" }}>
+        <LinearProgress />
+      </Box>
+    )
+
+  const getRows = (records: Record[] | undefined) => {
+    if (!records) return []
+
+    return records.map((r, index) =>
+      createData(
+        `${r.user?.lastName} ${r.user?.firstName}`,
+        r.user?.email || "",
+        10,
+        5,
+        80
+      )
+    )
+  }
 
   return (
     <Box>
@@ -37,9 +69,14 @@ function ResultPage() {
         BẢNG KẾT QUẢ
       </Typography>
       <Box sx={{ display: "flex", justifyContent: "space-between", my: 2 }}>
-        <Typography variant="h6">Phòng thi: {roomStore.selectedItem?.code || ""}</Typography>
-        <Typography variant="h6">Bộ đề: {roomStore.selectedItem?.quizCollection?.name || ""}</Typography>
-        <Typography variant="h6">Người tổ chức: GV. {roomStore.selectedItem?.owner?.lastName || ""} {roomStore.selectedItem?.owner?.firstName || ""}</Typography>
+        <Typography variant="h6">Phòng thi: {room.code || ""}</Typography>
+        <Typography variant="h6">
+          Bộ đề: {room.quizCollection?.name || ""}
+        </Typography>
+        <Typography variant="h6">
+          Người tổ chức: GV. {room.owner?.lastName || ""}{" "}
+          {room.owner?.firstName || ""}
+        </Typography>
       </Box>
       <Box>
         <TableContainer component={Paper} sx={{ backgroundColor: "#ffe7f0" }}>
@@ -60,21 +97,22 @@ function ResultPage() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {roomStore.selectedItem && getRows(roomStore.selectedItem.users).map((row) => (
-                // ! Có thể đổi lại key
-                <TableRow
-                  key={row.fullName}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.fullName}
-                  </TableCell>
-                  <TableCell>{row.email}</TableCell>
-                  <TableCell align="right">{row.rightAnswer}</TableCell>
-                  <TableCell align="right">{row.wrongAnswer}</TableCell>
-                  <TableCell align="right">{row.totalScore}</TableCell>
-                </TableRow>
-              ))}
+              {roomStore.selectedItem &&
+                getRows(roomStore.selectedItem.records).map((row) => (
+                  // ! Có thể đổi lại key
+                  <TableRow
+                    key={row.fullName}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.fullName}
+                    </TableCell>
+                    <TableCell>{row.email}</TableCell>
+                    <TableCell align="right">{row.rightAnswer}</TableCell>
+                    <TableCell align="right">{row.wrongAnswer}</TableCell>
+                    <TableCell align="right">{row.totalScore}</TableCell>
+                  </TableRow>
+                ))}
             </TableBody>
           </Table>
         </TableContainer>
