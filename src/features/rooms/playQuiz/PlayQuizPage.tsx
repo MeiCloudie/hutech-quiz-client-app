aa3cc5713dabf11b08be288363d274c8c936ab13
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-01-02 09:49:48 +0700

    feat(quiz): Update data for quiz

--- src/features/rooms/playQuiz/PlayQuizPage.tsx
+++ src/features/rooms/playQuiz/PlayQuizPage.tsx
@@ -1,10 +1,12 @@
 import { Box, Button, Divider, Grid, Typography } from "@mui/material"
 import useMediaQuery from "@mui/material/useMediaQuery"
 import { useTheme } from "@mui/material/styles"
+import { useStore } from "../../../app/stores/store"
 
 function PlayQuizPage() {
   const theme = useTheme()
   const isScreenSmall = useMediaQuery(theme.breakpoints.down("sm"))
+  const { roomStore } = useStore()
 
   const renderButtons = (buttonsConfig: { label: string; xs: number }[]) => {
     return buttonsConfig.map((config, index) => (
@@ -29,16 +31,40 @@ function PlayQuizPage() {
 
   const buttonsConfig = isScreenSmall
     ? [
-        { label: "A: Button 1", xs: 12 },
-        { label: "B: Button 2", xs: 12 },
-        { label: "C: Button 3", xs: 12 },
-        { label: "D: Button 4", xs: 12 },
+        {
+          label: `A: ${roomStore.selectedItem?.currentQuiz?.answers[0].content}`,
+          xs: 12,
+        },
+        {
+          label: `B: ${roomStore.selectedItem?.currentQuiz?.answers[1].content}`,
+          xs: 12,
+        },
+        {
+          label: `C: ${roomStore.selectedItem?.currentQuiz?.answers[2].content}`,
+          xs: 12,
+        },
+        {
+          label: `D: ${roomStore.selectedItem?.currentQuiz?.answers[3].content}`,
+          xs: 12,
+        },
       ]
     : [
-        { label: "A: Button 1", xs: 6 },
-        { label: "B: Button 2", xs: 6 },
-        { label: "C: Button 3", xs: 6 },
-        { label: "D: Button 4", xs: 6 },
+        {
+          label: `A: ${roomStore.selectedItem?.currentQuiz?.answers[0].content}`,
+          xs: 6,
+        },
+        {
+          label: `B: ${roomStore.selectedItem?.currentQuiz?.answers[1].content}`,
+          xs: 6,
+        },
+        {
+          label: `C: ${roomStore.selectedItem?.currentQuiz?.answers[2].content}`,
+          xs: 6,
+        },
+        {
+          label: `D: ${roomStore.selectedItem?.currentQuiz?.answers[3].content}`,
+          xs: 6,
+        },
       ]
 
   return (
@@ -54,12 +80,11 @@ function PlayQuizPage() {
       {/* score */}
       <Box sx={{ display: "flex", flexDirection: "column" }}>
         <Typography variant="h2" fontWeight={"bold"} gutterBottom>
-          Content cau hoi o day ne?
+          {roomStore.selectedItem?.currentQuiz?.content}
         </Typography>
-        <Typography
-          variant="h4"
-          color={"gray"}
-        >{`(Điểm: +${"score"})`}</Typography>
+        <Typography variant="h4" color={"gray"}>{`(Điểm: +${
+          roomStore.selectedItem?.currentQuiz?.score || ""
+        })`}</Typography>
       </Box>
 
       <Divider sx={{ my: 4 }} />
