132e614683ea88cf9b2e0759f660924a329c8122
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-03-24 21:57:34 +0700

    feat: Add validate for QuizData Form

--- src/features/generateQuiz/QuizData.tsx
+++ src/features/generateQuiz/QuizData.tsx
@@ -8,15 +8,28 @@ import {
   InputLabel,
   Select,
   TextField,
+  Button,
 } from "@mui/material"
 import React from "react"
 import { Field, Form, Formik } from "formik"
+import * as yup from "yup"
 
 interface QuizDataProps {
   quizData: any[]
 }
 
 const QuizData: React.FC<QuizDataProps> = ({ quizData }) => {
+  const validationSchema = yup.object({
+    content: yup.string().required("Nội dung không được để trống"),
+    explaination: yup.string().required("Giải thích không được để trống"),
+    answers: yup.array().of(
+      yup.object().shape({
+        content: yup.string().required("Câu trả lời không được để trống"),
+        isCorrect: yup.string().required("Vui lòng chọn đúng/sai"),
+      })
+    ),
+  })
+
   return (
     <Box>
       {quizData.length > 0 && (
@@ -52,77 +65,84 @@ const QuizData: React.FC<QuizDataProps> = ({ quizData }) => {
                   isCorrect: answer.isCorrect ? "correct" : "incorrect",
                 })),
               }}
+              validationSchema={validationSchema}
               onSubmit={(values) => {
                 console.log(values)
                 // Handle form submission here
               }}
             >
-              <Form>
-                <Field
-                  as={TextField}
-                  fullWidth
-                  variant="outlined"
-                  label="Nội dung"
-                  name="content"
-                  margin="normal"
-                  multiline
-                  rows={3}
-                />
-                <Field
-                  as={TextField}
-                  fullWidth
-                  variant="outlined"
-                  label="Giải thích"
-                  name="explaination"
-                  margin="normal"
-                  multiline
-                  rows={3}
-                />
-                <Grid container spacing={2}>
-                  {quiz.answers.map((answer: any, i: number) => (
-                    <React.Fragment key={i}>
-                      <Grid item xs={6}>
-                        <Field
-                          as={TextField}
-                          fullWidth
-                          variant="outlined"
-                          label={`Câu trả lời ${i + 1}`}
-                          name={`answers.${i}.content`}
-                          margin="normal"
-                        />
-                      </Grid>
-                      <Grid item xs={6}>
-                        <Field
-                          as={FormControl}
-                          fullWidth
-                          select
-                          variant="outlined"
-                          margin="normal"
-                        >
-                          <InputLabel
-                            id="select-label"
-                            htmlFor={`answers.${i}.isCorrect`}
-                          >
-                            Đúng / Sai
-                          </InputLabel>
+              {({ errors, touched }) => (
+                <Form>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    variant="outlined"
+                    label="Nội dung"
+                    name="content"
+                    margin="normal"
+                    multiline
+                    rows={3}
+                    error={touched.content && !!errors.content}
+                    helperText={touched.content && errors.content}
+                  />
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    variant="outlined"
+                    label="Giải thích"
+                    name="explaination"
+                    margin="normal"
+                    multiline
+                    rows={3}
+                    error={touched.explaination && !!errors.explaination}
+                    helperText={touched.explaination && errors.explaination}
+                  />
+                  <Grid container spacing={2}>
+                    {quiz.answers.map((answer: any, i: number) => (
+                      <React.Fragment key={i}>
+                        <Grid item xs={6}>
                           <Field
-                            as={Select}
-                            name={`answers.${i}.isCorrect`}
-                            labelId="select-label"
-                            label="Đúng / Sai"
-                            defaultValue={
-                              answer.isCorrect ? "correct" : "incorrect"
-                            }
+                            as={TextField}
+                            fullWidth
+                            variant="outlined"
+                            label={`Câu trả lời ${i + 1}`}
+                            name={`answers.${i}.content`}
+                            margin="normal"
+                          />
+                        </Grid>
+                        <Grid item xs={6}>
+                          <Field
+                            as={FormControl}
+                            fullWidth
+                            variant="outlined"
+                            margin="normal"
                           >
-                            <MenuItem value="correct">Correct</MenuItem>
-                            <MenuItem value="incorrect">Incorrect</MenuItem>
+                            <InputLabel
+                              id={`select-label-${i}`}
+                              htmlFor={`answers.${i}.isCorrect`}
+                            >
+                              Đúng / Sai
+                            </InputLabel>
+                            <Field
+                              as={Select}
+                              name={`answers.${i}.isCorrect`}
+                              labelId={`select-label-${i}`}
+                              label="Đúng / Sai"
+                              defaultValue={
+                                answer.isCorrect ? "correct" : "incorrect"
+                              }
+                            >
+                              <MenuItem value="correct">Correct</MenuItem>
+                              <MenuItem value="incorrect">Incorrect</MenuItem>
+                            </Field>
                           </Field>
-                        </Field>
-                      </Grid>
-                    </React.Fragment>
-                  ))}
-                </Grid>
-              </Form>
+                        </Grid>
+                      </React.Fragment>
+                    ))}
+                  </Grid>
+                  
+                </Form>
+              )}
             </Formik>
             <Divider sx={{ my: 2 }} />
           </div>
